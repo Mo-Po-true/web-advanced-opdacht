1976ac1c789fc3fe719035cc9d2cff7e453ed7b4
Mo-Po-true <mohamed.tijani@student.ehb.be>
2024-05-28 20:18:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ document.addEventListener('DOMContentLoaded', () => {
         modal.classList.remove('active-modal');
     });
 });
+
+/* deel 2- oranje */
+document.getElementById('submitBtn').addEventListener('click', function() {
+    const { value: username } = document.getElementById('username');
+    const { value: email } = document.getElementById('email');
+    const { value: password } = document.getElementById('password');
+
+    const fields = [username.trim(), email.trim(), password.trim()];
+    const emptyFields = fields.filter(field => field === '');
+
+    if (emptyFields.length > 0) {
+        alert('Vul alstublieft alle velden in.');
+    } else {
+        const [user, mail, pass] = fields;
+        localStorage.setItem('username', user);
+        localStorage.setItem('email', mail);
+        localStorage.setItem('password', pass);
+
+        alert('Formulier succesvol ingediend!');
+    }
+});
